af8a989dd5ab199732a167467cfa082330b49f89
Lilit Maralyan <97308627+LilitMaralyan@users.noreply.github.com>
2022-02-08 16:31:54 +0400

    Delete lilit/HW directory

--- lilit/HW/lesson1.py
+++
@@ -1,7 +0,0 @@
-"""Sentence with inputs"""
-# name = input("your name")
-# age = int(input("your age"))
-# print("Hello", name, "I am", age, "years old")
-
-"""backslash n and t"""
-# print('Python is powerful... and fast; \n\tplays well with others; \nruns everywhere;is friendly & easy to learn')
